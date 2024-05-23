1c8a6d5a4c2716aae0f6926009e7881905a452f0
Noussaiba <166798784+Noussai24@users.noreply.github.com>
2024-05-23 10:45:07 +0200

    Update calculator.h

--- include/calculator.h
+++ include/calculator.h
@@ -1,4 +1,13 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int car(int x); // Ajout de la déclaration de la fonction car
 double _add(double a, double b);
 double _sub(double a, double b);
 double _mul(double a, double b);
 double _div(double a, double b);
+#endif
